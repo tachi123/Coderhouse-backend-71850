d9ab40070f19ca20f40eed8422ad25b08d32f758
Jaime <jnramirezlorca@gmail.com>
2025-01-06 20:28:34 -0300

    primer commit

---
+++ Clase-00/index.js
@@ -0,0 +1,11 @@
+//Impresión en consola
+console.log("texto");
+console.warn("texto con advertencia");
+console.error("texto con error");
+
+console.log('abc');
+console.log(1);
+console.log([1,2,3]);
+console.log(true);
+console.log(null);
+console.log(undefined);
---
+++ Clase-00/variables.js
@@ -0,0 +1,21 @@
+//Definición de variables
+let nombre = 'Nahuel';
+let edad = 25;
+let precio = 50.75;
+const series = ["Breaking bad","One piece"];
+
+//Mostrar valores por consola
+console.log("Nombre: ", nombre);
+console.log("Edad: ", edad);
+console.log("Precio: ", precio);
+console.log("Series: ", series);
+
+//Actualizo valores
+edad++;// "S/D";
+//series.push();
+series.pop();
+
+//Mostrar valores actualizados por consola
+console.log("------ DESPUÉS DE LAS ACTUALIZACIONES ----");
+console.log("Edad: ", edad);
+console.log("Series: ", series);

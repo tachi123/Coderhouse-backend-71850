0b0ba62b691a7c108b7fd20c7728305888eb27af
Juli <jnramirezlorca@gmail.com>
2025-02-12 20:45:52 -0300

    subo protitpo final de la clase y configuración para que ande en glitch

--- Clase-11/ProyectoChat/src/app.js
+++ Clase-11/ProyectoChat/src/app.js
@@ -25,3 +25,27 @@ app.use(express.static(__dirname + '/public'));
 
 //Implementamos los routers
 app.use('/', viewsRouter);
+
+/**
+ * Empezamos a trababajar con el servidor socket
+ * Nos ponemos a escuchar conexiones
+ */
+const messages = []; //Los mensajes se almacenaran aquí
+io.on('connection', socket => {
+    console.log(`Nuevo cliente conectado: ${socket.id}`);
+
+    socket.on('userAuthenticated', user => {
+        //Emitir los logs del chat al usuario que se acaba de autenticar
+        socket.emit('messageLogs', messages);
+        //Emitir una notificación a todos los demás usuarios
+        socket.broadcast.emit('newUserConnected', user);
+    })
+
+    //Recibir mensajes y enviarlos a todos los clientes
+    socket.on('message', data => {//Escuchamos el evento con el mismo nombre que emite el cliente: "message"
+        messages.push(data); //Guardamos el mensaje en la "base"
+        console.log(messages);
+        io.emit('messageLogs', messages); //Reenviar instántaneamente los logs actualizados a todos los CLIENTES
+    })
+
+})
--- Clase-11/ProyectoChat/src/public/js/index.js
+++ Clase-11/ProyectoChat/src/public/js/index.js
@@ -6,7 +6,9 @@ const socket = io();
  * Nos permite instanciar el socket y guardarlo en la constante "socket"
  * Este socket es el que vamos a utilizar par poder comunicarnos con el socket del servidor
  */
-
+//Creación de elementos del DOM y variables auxiliares
+let chatBox = document.getElementById('chatBox'); //Obtenemos la referencia del cuadro donde se escribirá.
+let user; //Este "user" será con el que el cliente se identificará para saber quién escribió el mensaje
 //Alerta de identificación
 Swal.fire({
     title: "Identifícate",
@@ -18,6 +20,41 @@ Swal.fire({
     },
     allowOutsideClick: false, //Impedimos que el usuario salga de la alerta al dar el "click" fuera de la alerta
 }).then( result => {
+    //Guardamos en la variable user el valor ingresado por el usuario
+    user = result.value;
+    //Una vez que el usuario se identifica, le asignamos la variable User al objeto HTML con id 'username-display'
+    document.getElementById('username-display').textContent = user; 
+    //Emitir un evento avisando que se conecto un nuevo usuario
+    socket.emit('userAuthenticated', {user: user});
+})
 
-    document.getElementById('username-display').textContent = result.value; 
+//Eventlistener para el input del chat
+chatBox.addEventListener('keyup', (evt) => {
+    if(evt.key === 'Enter'){//El mensaje se enviará cuando el usuario aprete "Enter" en la caja del chat
+        if(chatBox.value.trim().length){//Corroboramos que el mensaje no este vacío o sólo contenga espacios
+            socket.emit('message', {user: user, message: chatBox.value}); //Emitimos nuestro evento mensaje
+            chatBox.value = '';
+        }   
+    }
+})
+
+//Escuchar el evento 'messageLogs' así actualizamos la lista de mensajes (pisamos los mensajes)
+socket.on('messageLogs', data => {//data es el array con todos los mensajes
+    let elementLog = document.getElementById('messageLogs');
+    let messageHtml = "";
+    data.forEach( message => messageHtml += `${message.user} dice: ${message.message}<br>`);
+    elementLog.innerHTML = messageHtml;
+})
+
+//Cuando alguien se conecta, muestro una alerta usando SweetAlert2
+socket.on('newUserConnected', newUser => {
+    Swal.fire({
+        text: "Nuevo usuario conectado",
+        toast: true,
+        position: "top-right",
+        showConfirmButton: false,
+        timer: 5000, //en milisegundos, sería 5 segundos
+        icon: 'info',
+        title: `${newUser.user} se ha unido al chat`
+    })
 })
